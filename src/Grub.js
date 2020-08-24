e9abc50a05a29a60e4b70200d0a831d8f461fa67
tiffchannn <tiffany.yi.chan@gmail.com>
2020-08-24 15:34:57 -0700

    Updated eat function

--- src/Grub.js
+++ src/Grub.js
@@ -8,10 +8,11 @@ var Grub = function() {
   this.age = 0;
   this.color = 'pink';
   this.food = 'jelly';
-  this.eat = function ();
 };
 
-// Grub.protoype.eat = function() {
-// };
+Grub.prototype.eat = function() {
+
+};
+
 
 
