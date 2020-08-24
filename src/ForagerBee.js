5dddb2941a7ab62f876bbee8c532362a66023c33
tiffchannn <tiffany.yi.chan@gmail.com>
2020-08-24 15:35:18 -0700

    Successfully passed all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,15 +1,4 @@
 var ForagerBee = function() {
-/*
- call the Bee superclass
- an age property that is set to 10
- a job property that is set to find pollen
- a color property inherited from bee that is set to yellow
- a food property that is inherited from grub
- an eat method that is inherited from grub
- a canFly property that is set true
- a treasureChest property that is set to an empty array []
-
-*/
 
   Bee.call(this);
   this.age = 10;
@@ -20,14 +9,10 @@ var ForagerBee = function() {
 
 };
 
-// set protoype
-ForagerBee.prototype = Object.create(Bee.protoype);
-
-// set constructor
-ForagerBee.prototype.constructor = ForagerBee
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
 
 // add forage method that allows the bee to add a treasure to the treasureChest
 ForagerBee.prototype.forage = function(treasure) {
   this.treasureChest.push(treasure);
 }
-
