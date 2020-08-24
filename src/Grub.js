343026894c4e64f8714b7373f0cf6f40c0023329
tiffchannn <tiffany.yi.chan@gmail.com>
2020-08-24 11:55:03 -0700

    Implemented age, color, food properties and eat method to Grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,16 @@
 var Grub = function() {
+  // pseudoclassical style, with:
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  this.eat = function();
 };
 
+Grub.protoype.eat = function() {
+
+};
+
+ // age property that is set to 0
+  // color property that is set to pink
+  // food property that is set to jelly
+  //eat method
