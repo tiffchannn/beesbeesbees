3735ea65b53b47d7c5a41f27ccdc71db6c845679
tiffchannn <tiffany.yi.chan@gmail.com>
2020-08-24 11:56:24 -0700

    Implemented call superclass, set constructor and protoype. Still working on inheritance from Grub

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,26 @@
 var Bee = function() {
+  // missing input for constructor function?
+  /*
+  call the Grub superclass
+  set the prototype
+  set the constructor
+  an age property that is set to 5
+  a color property that is set to yellow
+  a food property that is inherited from grub
+  an eat method that is inherited from grub
+  a job property that is set to keep on growing
+  */
+
+  Grub.call(this, age, color, food, eat);
+
 };
 
+// set the prototype - will delegate all failed lookups on Bee to Grub to find shared methods stored there
+Bee.protoype= Object.create(Grub.prototype);
+
+// set the constructor - creates a delegation relationship; all other bees will report to this constructor
+Bee.prototype.constructor = Bee.protoype;
+
+Bee.protoype.job = function() {
+
+};
